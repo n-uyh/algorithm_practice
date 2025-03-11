073284f3a8caccf6d6705490779ca0e527c79df4
n__u.yh <edocnuyh@gmail.com>
2025-03-11 11:42:14 +0900

    Title: 변수 출력하기 3, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250311/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+        String arrow = "->";
+        System.out.println(a + arrow + b + arrow + c);
+    }
+}
