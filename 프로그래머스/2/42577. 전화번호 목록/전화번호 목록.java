d77d0a4fcb49b1f287fdab3b0119182ea25c4bae
n__u.yh <edocnuyh@gmail.com>
2025-06-24 01:02:47 +0900

    [level 2] Title: 전화번호 목록, Time: 350.83 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,25 +2,10 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호의 시작번호로 색인을 만들자.
-        Map<String, List<String>> phones = new HashMap<>();
+        Arrays.sort(phone_book);
         
-        for (String p : phone_book) {
-            String firstNum = p.charAt(0) + "";
-            List<String> numbers = phones.getOrDefault(firstNum, new ArrayList<>());
-            numbers.add(p);
-            phones.put(firstNum, numbers);
-        }
-        
-        // 같은번호로 시작하는 것 끼리 탐색
-        for (String key : phones.keySet()) {
-            List<String> numbers = phones.get(key);
-            Collections.sort(numbers);
-            for (int i = 1; i < numbers.size(); i++) {
-                if (numbers.get(i).startsWith(numbers.get(i-1))) {
-                    return false;
-                }
-            }
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].indexOf(phone_book[i]) == 0) return false;
         }
         
         return true;
