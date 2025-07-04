8ce81de51f542b28c6e2b0c5dceee44ba1d28e1e
n__u.yh <edocnuyh@gmail.com>
2025-07-04 23:24:55 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 178ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 250704/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
+++ 250704/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -3,8 +3,9 @@ public class Main {
     static int n, m, t;
     static int[][] grid, marbles;
 
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
+    // 상하좌우
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
