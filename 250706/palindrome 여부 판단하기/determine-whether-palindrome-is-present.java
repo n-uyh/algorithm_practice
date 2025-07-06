9ce773823a1b7a641bc0421cf8919401777a5d5f
n__u.yh <edocnuyh@gmail.com>
2025-07-06 09:38:43 +0900

    Title: palindrome 여부 판단하기, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250706/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
+++ 250706/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -4,16 +4,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
 
-        boolean isPalindrome = input.equals(flip(input));
-        System.out.print(isPalindrome ? "Yes" : "No");
+        System.out.print(isPalindrome(input) ? "Yes" : "No");
     }
 
-    public static String flip(String target) {
-        String flipped = "";
-        int len = target.length() - 1;
-        for (int i = 0; i <= len; i++) {
-            flipped += target.charAt(len - i);
+    public static boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
         }
-        return flipped;
+        return true;
     }
 }
