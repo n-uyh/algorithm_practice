7dc7a08b9a5fa4eb8bcf21983380292505c8080a
n__u.yh <edocnuyh@gmail.com>
2025-07-26 14:51:20 +0900

    Title: 최소 정수 출력, Time: 255ms, Memory: 29MB, Status: Passed - Codetree

--- 250726/최소 정수 출력/min-integer-output.py
+++ 250726/최소 정수 출력/min-integer-output.py
@@ -4,13 +4,12 @@ n = int(input())
 x = [int(input()) for _ in range(n)]
 pq = []
 
-for i,el in enumerate(x):
+for el in x:
     if el > 0:
         heapq.heappush(pq, el)
     else:
-        if (i == 0):
+        try:
+            print(heapq.heappop(pq))
+        except IndexError:
             print(0)
-        else:
-            min = heapq.heappop(pq)
-            print(min)
 
